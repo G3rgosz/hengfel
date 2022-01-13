7b211eff3532f6b78f45dd645ea0df82ec5e6eea
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2022-01-13 10:52:48 +0100

    az alkalmazás működik, kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,12 @@
+/*
+* File: app.component.ts
+* Author: Szivák Gergő
+* Copyright: 2022, Szivák Gergő
+* Group: Szoft II/N
+* Date: 2022-01-13
+* Github: https://github.com/Gergosz-2000/hengfel
+* Licenc: GNU GPL
+*/
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +16,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hengfel';
+  radius: string;
+  length: string;
+  surface: string;
+  constructor(){
+    this.radius = '';
+    this.length = '';
+    this.surface = '';
+  }
+  calcSurface(): any {
+    this.surface = "2π"+this.radius+"("+this.radius+"+"+this.length+")"+"="+String((2*Math.PI*Number(this.radius)*(Number(this.radius)+Number(this.length))).toFixed(2));
+    this.radius = '';
+    this.length = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,14 @@
+/*
+* File: app.module.ts
+* Author: Szivák Gergő
+* Copyright: 2022, Szivák Gergő
+* Group: Szoft II/N
+* Date: 2022-01-13
+* Github: https://github.com/Gergosz-2000/hengfel
+* Licenc: GNU GPL
+*/
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +18,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
